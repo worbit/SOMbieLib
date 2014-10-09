abc2b3f6886915fd1215c79432df3491f9f3f2a8
worbit <bernhard@arch.ethz.ch>
2014-10-09 16:05:22 +0200

    cleaned Applet

--- SOMbie/src/app/SombieApplet.java
+++ SOMbie/src/app/SombieApplet.java
@@ -53,32 +53,6 @@ public class SombieApplet extends PApplet {
 	    fill(r, g, b);
 	    rect(i % nx * tx, i / nx * ty, tx, ty);
 	}
-	// for (int i=0; i<nx*ny; i++) {
-	// PImage pic = createImage(4,4, RGB);
-	// pic.loadPixels();
-	// SombieVector sv = sl.getNode(i).getVector();
-	//
-	// int d = color(0);
-	// for (int j=0; j<pic.pixels.length; j++) pic.pixels[j] = color(255);
-	// for (int j=0; j<dim; j++) {
-	// boolean b = sv.get(j).doubleValue()>0.5;
-	// int x = j%3;
-	// int y = j/3;
-	// int index = (y+1)*8 + x+1;
-	// int ind2 = (y+1)*8 + (6-x);
-	// if (b) {
-	// pic.pixels[index] = color(255);
-	// pic.pixels[ind2] = color(255);
-	// }
-	// else {
-	// pic.pixels[index] = d;
-	// pic.pixels[ind2] = d;
-	// }
-	// }
-	// pic.updatePixels();
-	// image(pic,i%nx*tx,i/nx*ty,tx,ty);
-	// }
-
     }
 
     public void keyPressed() {
